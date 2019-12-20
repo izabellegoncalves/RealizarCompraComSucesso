9431d3ab74d55be92706b6aa0a06ba06ef435471
izabellegoncalves <59073534+izabellegoncalves@users.noreply.github.com>
2019-12-20 00:07:14 -0300

    Add files via upload

---
+++ e2e-cypress/cypress/plugins/index.js
@@ -0,0 +1,27 @@
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+const path = require('path');
+module.exports = (on, config) => {
+  // eslint-disable-next-line consistent-return
+  on('before:browser:launch', (browser = {}, args) => {
+      console.log(config, browser, args);
+      if (browser.name === 'chrome') {
+        const ignoreXFrameHeadersExtension = path.join(__dirname, '../extensions/ignore-x-frame-headers');
+        args.push(args.push(`--load-extension=${ignoreXFrameHeadersExtension}`));
+        args.push("--disable-features=CrossSiteDocumentBlockingIfIsolating,CrossSiteDocumentBlockingAlways,IsolateOrigins,site-per-process");
+      }
+      return args; 
+  });
+
+};
